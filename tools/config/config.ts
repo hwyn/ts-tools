3cadbf49828c1ecf9316fa194193020d07a48d10
Xiang Ni <xiang_x_ni@mfcgd.com>
2019-06-12 12:24:18 +0800

    init

--- tools/config/config.ts
+++ tools/config/config.ts
@@ -1,10 +1,19 @@
 import path from 'path';
+import { requireSync } from '../core/fs';
 import pkg from '../../package.json';
 
 const isDebug = !process.argv.includes('--release');
 const baseDir: string = process.cwd();
 const resolve = (base: string) => (_path: string) => path.resolve(base, _path);
 const baseResolve = resolve(baseDir);
+const processPkg = requireSync(`${baseDir}/package.json`);
+let mergePackage = Object.assign({}, {
+  babellrc: pkg.babel,
+  browserslist: pkg.browserslist,
+}, processPkg ? {
+  babellrc: processPkg.babel,
+  browserslist: processPkg.browserslist,
+} : {});
 
 export default {
   baseDir,
@@ -12,6 +21,6 @@ export default {
   srcDir: baseResolve('src'),
   buildDir: baseResolve('build'),
   distDir: baseResolve('dist'),
-  babellrc: pkg.babel,
-  browserslist: pkg.browserslist,
+  babellrc: mergePackage.babellrc,
+  browserslist: mergePackage.browserslist,
 };
