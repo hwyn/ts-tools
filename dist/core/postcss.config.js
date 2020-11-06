5fbbd1877598d46bcbe36e4bf4452c1fd118a9aa
xiangni <xiangni@mfcgd.com>
2020-11-06 17:53:11 +0800

    update package

--- dist/core/postcss.config.js
+++ dist/core/postcss.config.js
@@ -1,11 +1,11 @@
 "use strict"; /**
-               * React Starter Kit (https://www.reactstarterkit.com/)
-               *
-               * Copyright © 2014-present Kriasoft, LLC. All rights reserved.
-               *
-               * This source code is licensed under the MIT license found in the
-               * LICENSE.txt file in the root directory of this source tree.
-               */
+ * React Starter Kit (https://www.reactstarterkit.com/)
+ *
+ * Copyright © 2014-present Kriasoft, LLC. All rights reserved.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE.txt file in the root directory of this source tree.
+ */
 
 /* eslint-disable global-require */
 const { config } = require('../config');
