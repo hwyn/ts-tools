bd408fa565a26237ed0e8b06d59e727ba39db274
xiang ni <xiang_x_ni@manulife.com>
2020-12-26 11:39:45 +0800

    update

--- dist/config/dll/webpack.prod.config.js
+++ dist/config/dll/webpack.prod.config.js
@@ -1,4 +1,4 @@
-"use strict";Object.defineProperty(exports, "__esModule", { value: true });exports.default = void 0;
+"use strict";Object.defineProperty(exports, "__esModule", { value: true });exports.default = void 0;var _webpack = require("webpack");
 var _webpackMerge = _interopRequireDefault(require("webpack-merge"));
 var _uglifyjsWebpackPlugin = _interopRequireDefault(require("uglifyjs-webpack-plugin"));
 var _webpackBase = _interopRequireDefault(require("./webpack.base.config"));
@@ -8,7 +8,29 @@ const { isDevelopment } = (0, _config.platformConfig)();var _default =
 
 () => (0, _webpackMerge.default)((0, _webpackBase.default)(), {
   optimization: {
+    splitChunks: {
+      chunks: 'async',
+      minSize: 20000,
+      maxSize: 0,
+      minChunks: 1,
+      maxAsyncRequests: 30,
+      maxInitialRequests: 30,
+      automaticNameDelimiter: '~',
+      cacheGroups: {
+        defaultVendors: {
+          test: /[\\/]node_modules[\\/]/,
+          priority: -10,
+          reuseExistingChunk: true },
+
+        default: {
+          minChunks: 2,
+          priority: -20,
+          reuseExistingChunk: true } } },
+
+
+
     minimizer: [
+    new _webpack.HashedModuleIdsPlugin(),
     new _uglifyjsWebpackPlugin.default({
       sourceMap: isDevelopment,
       cache: true,
--- tools/config/dll/webpack.prod.config.ts
+++ tools/config/dll/webpack.prod.config.ts
@@ -1,4 +1,4 @@
-import { Configuration } from 'webpack';
+import { Configuration, HashedModuleIdsPlugin } from 'webpack';
 import merge from 'webpack-merge';
 import UglifyJSPlugin from 'uglifyjs-webpack-plugin';
 import baseConfig from './webpack.base.config';
@@ -8,7 +8,29 @@ const { isDevelopment } = platformConfig();
 
 export default (): Configuration => merge(baseConfig(), {
   optimization: {
+    splitChunks: {
+      chunks: 'async',
+      minSize: 20000,
+      maxSize: 0,
+      minChunks: 1,
+      maxAsyncRequests: 30,
+      maxInitialRequests: 30,
+      automaticNameDelimiter: '~',
+      cacheGroups: {
+        defaultVendors: {
+          test: /[\\/]node_modules[\\/]/,
+          priority: -10,
+          reuseExistingChunk: true
+        },
+        default: {
+          minChunks: 2,
+          priority: -20,
+          reuseExistingChunk: true
+        }
+      }
+    },
     minimizer: [
+      new HashedModuleIdsPlugin(),
       new UglifyJSPlugin({
         sourceMap: isDevelopment,
         cache: true,
@@ -27,6 +49,6 @@ export default (): Configuration => merge(baseConfig(), {
           }
         }
       }),
-    ],
+    ]
   },
 });
