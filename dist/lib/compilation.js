524c4160b7da1817497053b5b266f2479de30f7d
xiang ni <xiang_x_ni@manulife.com>
2021-11-01 00:31:57 +0800

    update

--- dist/lib/compilation.js
+++ dist/lib/compilation.js
@@ -13,7 +13,6 @@ function createCompilationPromise(name, compiler, config) {
             console.info(`[${format(timeStart)}] Compiling '${name}'...`);
         });
         compiler.hooks.done.tap(name, (stats) => {
-            console.info(stats.toString(config.stats));
             const timeEnd = new Date();
             const time = timeEnd.getTime() - timeStart.getTime();
             if (stats.hasErrors()) {
@@ -22,7 +21,7 @@ function createCompilationPromise(name, compiler, config) {
                 reject(new Error('Compilation failed!'));
             }
             else {
-                console.info(`[${format(timeEnd)}] Finished '${name}' compilation after ${time} ms`);
+                // console.info(`[${format(timeEnd, )}] Finished '${name}' compilation after ${time} ms`);
                 resolve(stats);
             }
         });
