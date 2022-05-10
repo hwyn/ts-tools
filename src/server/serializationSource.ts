5a9a1987403499376e3ea0647d58695bf15f753d
xiang ni <xiang_x_ni@manulife.com>
2022-05-10 15:01:14 +0800

    update

---
+++ src/server/serializationSource.ts
@@ -0,0 +1,37 @@
+import fs, { existsSync } from 'fs';
+import path from 'path';
+
+export interface Source<T> {
+  javascript: Array<T>;
+  styleSheet: Array<T>;
+}
+
+type serialization = () => Source<string>;
+
+function getAssets() {
+  const filePath = path.join(__dirname, '../../build/assets.json');
+  if (!existsSync(filePath)) {
+    return {};
+  }
+  return JSON.parse(fs.readFileSync(filePath, { encoding: 'utf-8' }));
+}
+
+const serializationSource = (source: any) => ['runtime', 'polyfill', 'main'].reduce((o: Source<string>, key): Source<string> => {
+  let __source;
+  if (!key || !(__source = source[key])) {
+    return o as Source<string>;
+  }
+  const javascript = [].concat(__source.js || []).map((path: string) => `<script src="/${path}"></script>`);
+  const styleSheet = [].concat(__source.css || []).map((path: string) => `<link rel="stylesheet" href="/${path}">`);
+  return Object.assign({
+    javascript: o.javascript.concat(javascript),
+    styleSheet: o.styleSheet.concat(styleSheet),
+  });
+}, { javascript:[], styleSheet: [] } as Source<string>);
+
+const getResource = ((): serialization => {
+  let json: Source<string>;
+  return (): Source<string> => json ? json : json = serializationSource(getAssets());
+})();
+
+export default getResource;
